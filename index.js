6ed35107a2bc6099f01fc858a28169d7f7057c82
gift kimani <gift.kimani@students.moringaschool.com>
2023-06-24 22:47:01 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution here
+function findMatching(drivers, searchString) {
+    // Convert the search string to lowercase for case-insensitive matching
+    const searchLower = searchString.toLowerCase();
+  
+    // Filter the drivers array to find matching names
+    const matchingDrivers = drivers.filter(driver =>
+      driver.toLowerCase().includes(searchLower)
+    );
+  
+    return matchingDrivers;
+  }
+  
+  function fuzzyMatch(drivers, query) {
+    const matches = [];
+  
+    for (const driver of drivers) {
+      if (driver.toLowerCase().startsWith(query.toLowerCase())) {
+        matches.push(driver);
+      }
+    }
+  
+    return matches;
+  }
+
+  function matchName(drivers, query) {
+    const matches = [];
+  
+    for (const driver of drivers) {
+      if (driver.name.toLowerCase() === query.toLowerCase()) {
+        matches.push(driver);
+      }
+    }
+  
+    return matches;
+  }
